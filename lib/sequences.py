c60ab323af9dfea08978abda6fb31f1769fa8ec8
Kimberly-K04 <kkwamboka04@gmail.com>
2025-12-15 19:29:07 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
+def print_fibonacci(n):
+    fib = []
 
-def print_fibonacci(length):
-    pass
+    if n <= 0:
+        print(fib)
+        return
+
+    a, b = 0, 1
+    for _ in range(n):
+        fib.append(a)
+        a, b = b, a + b
+
+    print(fib)
